c7989885d551610ec5104cc58ee8660cc6fd6107
Arsyabot <95653453+Arsyabot@users.noreply.github.com>
2022-04-03 16:40:12 +0700

    Update chats.py

--- Music/MusicUtilities/database/chats.py
+++ Music/MusicUtilities/database/chats.py
@@ -2,6 +2,7 @@ from typing import Dict, Union, List
 from Music import db
 
 chatsdb = db.chats
+blacklist_chatdb = db.blacklistChat
 
 async def get_served_chats() -> list:
     chats = chatsdb.find({"chat_id": {'$lt': 0}})
@@ -24,17 +25,28 @@ async def add_served_chat(chat_id: int):
         return
     return await chatsdb.insert_one({"chat_id": chat_id})
  
-async def get_served_chats() -> list:
-    chats = chatsdb.find({"chat_id": {"$lt": 0}})
-    if not chats:
-        return []
-    chats_list = []
-    for chat in await chats.to_list(length=1000000000):
-        chats_list.append(chat)
-    return chats_list
 
 async def remove_served_chat(chat_id: int):
     is_served = await is_served_chat(chat_id)
     if not is_served:
         return
     return await chatsdb.delete_one({"chat_id": chat_id})
+
+
+async def blacklisted_chats() -> list:
+    chats = blacklist_chatdb.find({"chat_id": {"$lt": 0}})
+    return [chat["chat_id"] for chat in await chats.to_list(length=1000000000)]
+
+
+async def blacklist_chat(chat_id: int) -> bool:
+    if not await blacklist_chatdb.find_one({"chat_id": chat_id}):
+        await blacklist_chatdb.insert_one({"chat_id": chat_id})
+        return True
+    return False
+
+
+async def whitelist_chat(chat_id: int) -> bool:
+    if await blacklist_chatdb.find_one({"chat_id": chat_id}):
+        await blacklist_chatdb.delete_one({"chat_id": chat_id})
+        return True
+    return False
